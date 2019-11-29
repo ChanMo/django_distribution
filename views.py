55052f6609e16cc2fad2c173c3b10bfa931b2a49
dyou <chen.orange@aliyun.com>
2019-11-29 10:32:18 +0800

    add all

--- views.py
+++ views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.mixins import RetrieveModelMixin
+
+from .models import *
+from .serializers import *
+
+
+class AppApiView(RetrieveAPIView):
+    queryset = App.objects.all()
+    serializer_class = AppSerializer
+    lookup_field = 'slug'
+
+
+class AppView(DetailView):
+    model = App
+    lookup_field = 'slug'
+    template_name = 'distribution/index.html'
